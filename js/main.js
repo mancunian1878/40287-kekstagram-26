220ee64b701f75ba5c2d18d556cb409a1e8e2cee
Мурад <limatov.murad@yandex.ru>
2022-06-13 20:31:43 +0300

    задание 4.15. Больше деталей

--- js/main.js
+++ js/main.js
@@ -7,14 +7,50 @@ const getPositiveInteger = (min, max) => {
 
 };
 
+const MESSAGES = [
+  'Всё отлично!', 'В целом всё неплохо. Но не всё',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра', 'В конце концов это просто непрофессионально.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+const NAMES = ['Идрак', 'Санёк', 'Гарик', 'Лёха'];
+
+const createComment = (id) => ({
+  id,
+  avatar: `img/avatar-${getPositiveInteger(1, 6)}.svg`,
+  message: MESSAGES[getPositiveInteger(0, MESSAGES.length - 1)],
+  name: NAMES[getPositiveInteger(0, NAMES.length - 1)],
+});
+
+const createObject = (id) => {
+  const comments = [];
+  const commentsCount = getPositiveInteger(0, 7);
+
+  for (let comment = 1; comment <= commentsCount; comment++) {
+    comments.push(createComment(comment));
+  }
+
+  return {
+    id,
+    url: `photos/${id}.jpg`,
+    description: 'Мой первый опыт в фотографии',
+    likes: getPositiveInteger(15, 200),
+    comments,
+  };
+};
+
+const generateObjects = (count) => {
+  const objects = [];
+  for (let object = 1; object <= count; object++) {
+    objects.push(createObject(object));
+  }
+  return objects;
+};
+
+generateObjects(25);
+
+
 // Вычисление длины комментария
 
-const getMaxLengthString = (stringLength, stringLengthMax) => {
-  if (stringLength <= stringLengthMax) {
-    true;
-  }
-
-  else {
-    false;
-  }
+const checkStringLength = (string, stringLengthMax) => {
+  return string.length <= stringLengthMax;
 };
